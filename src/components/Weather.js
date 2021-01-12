5ea43e94915a8254d79c83c59aa81213a3476f15
Gary Barkauski <gbarka2@gmail.com>
2021-01-11 20:40:17 -0700

    Add text color change

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 const Weather = ({weather}) => {
-  // console.log('weather load', props.weather.name)
+  
+  const [color, setColor] = useState('white')
+  const handleColor = (temp) => {
+    if (temp < 40) {
+      setColor('blue')
+    } else if (temp > 90) {
+      setColor('red')
+    }
+  }
+
+
+
+  useEffect(() => {
+    handleColor(weather?.main?.temp)
+  },[weather?.main?.temp])
+
   const loaded = () => {
     return (
       <div>
         <p>{weather.name}</p>
-        <p>Current Temperature {weather.main.temp}</p>
+        <p style={{color: color}}>Current Temperature {weather.main.temp}</p>
         <p>{weather.weather[0].description}</p>
-        <p>Max Temp {weather.main.temp_max}</p>
-        <p>Min Temp {weather.main.temp_min}</p>
+        <p style={{color: color}}>Max Temp {weather.main.temp_max}</p>
+        <p style={{color: color}}>Min Temp {weather.main.temp_min}</p>
       </div>
     )
   }
